b24c04d907d8b296cdfb930e7560fa99c468006d
Richard Palmer <richeytastic@gmail.com>
2017-08-04 20:01:22 +0800

    Added GPL v3 license headers to .h and .cpp files.

--- include/AdaptiveDepthPatchScanner.h
+++ include/AdaptiveDepthPatchScanner.h
@@ -1,3 +1,20 @@
+/************************************************************************
+ * Copyright (C) 2017 Richard Palmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+
 /**
  * Scans a depth image in a continuous line given a real patch size, passing at each pixel
  * a rectangular patch calculated from this point according to the given dimensions.
